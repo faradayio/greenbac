3d5201c7e49568d62fd955193ab9ba78a490668d
Andy Rossmeissl <andy@rossmeissl.net>
2010-08-16 16:45:28 -0500

    rails new greenbac

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random, 
+# no regular words or you'll be exposed to dictionary attacks.
+Rails.application.config.secret_token = '75e9e00e4c46038edcf5fc991de05a6e521fee70f9d9b72d96d95403e178b665e4f9cf2fc29bccf3a473c3d780f2644e1aeb6f68f2cab912ccb5b97cb5c84973'
